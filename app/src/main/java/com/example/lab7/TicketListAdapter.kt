19215f99755d9ebb8263d3fe69086e5a09943998
abisara2k1 <abisara@iu.edu>
2025-02-27 12:38:46 -0500

    homework3-part5 completetd

--- app/src/main/java/com/example/lab7/TicketListAdapter.kt
+++ app/src/main/java/com/example/lab7/TicketListAdapter.kt
@@ -1,31 +1,32 @@
 package com.example.lab7
 
-import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lab7.databinding.ListItemTicketBinding
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+import java.util.UUID
 
 
 class TicketHolder(
-    private val binding: ListItemTicketBinding
+    private val binding: ListItemTicketBinding,
+    val onTicketClicked: (ticketId: UUID) -> Unit
 ) : RecyclerView.ViewHolder(binding.root) {
 
+    private val dateFormat = SimpleDateFormat("EEEE, dd MMM yyyy, HH:mm", Locale.getDefault())
     fun bind(ticket: Ticket) {
         binding.ticketTitle.text = ticket.title
 
-        val formattedDate = DateFormat.format("MMM dd, yyyy", Date(ticket.date)).toString()
-        binding.ticketDate.text = formattedDate 
+        val formattedDate = dateFormat.format(Date(ticket.date))
+        binding.ticketDate.text = formattedDate
 
         binding.root.setOnClickListener {
-            Toast.makeText(
-                binding.root.context,
-                "${ticket.title} clicked!",
-                Toast.LENGTH_SHORT
-            ).show()
+            onTicketClicked(ticket.id)
         }
 
         binding.ticketSolved.visibility = if (ticket.isSolved) {
@@ -34,16 +35,18 @@ class TicketHolder(
             View.GONE
         }
     }
+
 }
 
 class TicketListAdapter(
-    private val tickets: List<Ticket>
+    private val tickets: List<Ticket>,
+    private val onTicketClicked: (ticketId: UUID) -> Unit
 ) : RecyclerView.Adapter<TicketHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TicketHolder {
         val inflator = LayoutInflater.from(parent.context)
         val binding = ListItemTicketBinding.inflate(inflator, parent, false)
-        return TicketHolder(binding)
+        return TicketHolder(binding, onTicketClicked)
     }
 
     override fun getItemCount(): Int {
