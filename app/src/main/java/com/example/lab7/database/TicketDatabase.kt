c84bbb1e7c24ad762e894b2fafcfcc77117efb58
abisara2k1 <abisara@iu.edu>
2025-02-28 00:26:55 -0500

    homework3-part7-challenge completed

--- app/src/main/java/com/example/lab7/database/TicketDatabase.kt
+++ app/src/main/java/com/example/lab7/database/TicketDatabase.kt
@@ -1,9 +1,15 @@
 package com.example.lab7.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.lab7.Ticket
+import com.example.lab7.database.TicketTypeConverter
+
 
 @Database(entities = [Ticket::class], version = 1)
 @TypeConverters(TicketTypeConverter::class)
